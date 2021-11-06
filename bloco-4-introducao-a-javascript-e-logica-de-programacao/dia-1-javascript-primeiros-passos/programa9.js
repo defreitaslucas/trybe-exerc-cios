40ec9e14a68b9d67837c8a12ab94c54383aef37e
LucasTapa <lucas.dfa@live.com>
2021-11-06 12:31:30 -0300

    alteraçao no programa 9 melhorando o algoritmo

--- bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-1-javascript-primeiros-passos/programa9.js
+++ bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-1-javascript-primeiros-passos/programa9.js
@@ -2,7 +2,7 @@ const num1 = 0;
 const num2 = 4;
 const num3 = 11;
 
-if (num1%2 == 1 || num2%2 == 1 || num3%2 == 1) {
+if (num1%2 !== 0 || num2%2 !== 0 || num3%2 !== 0) {
     console.log(true);
 }
 else {
