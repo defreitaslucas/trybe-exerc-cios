f353923a2fac2cf961567c928b0078cfe48efa96
LucasTapa <lucas.dfa@live.com>
2021-11-08 15:53:16 -0300

    exerc_fixacao_termino

--- bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-2-javascript-array-e-loop-for/exerc_fixacao.js
+++ bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-2-javascript-array-e-loop-for/exerc_fixacao.js
@@ -9,6 +9,17 @@
 // console.log(indexOfPortfolio);
 
 
-let menu = ['Home', 'Serviços', 'Portfólio', 'Links'];
-menu.push('Contato')
-console.log(menu);
+// let menu = ['Home', 'Serviços', 'Portfólio', 'Links'];
+// menu.push('Contato')
+// console.log(menu);
+
+// let groceryList = ['Arroz', 'Feijão', 'Alface', 'Melancia'];
+
+// for(let index = 0; index < groceryList.length; index += 1){
+//     console.log(groceryList[index]);    
+// }
+
+let names = ['João', 'Maria', 'Antônio', 'Margarida'];
+for (let elementos of names){
+    console.log(elementos);
+}
