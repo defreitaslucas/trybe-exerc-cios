aa48ffb5f65f9d75008483725e897533129e1042
LucasTapa <lucas.dfa@live.com>
2021-11-08 16:24:36 -0300

    programa5for

---
+++ bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-2-javascript-array-e-loop-for/programa5for.js
@@ -0,0 +1,9 @@
+let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+let valorMaior = 0;
+
+for(let index = 0; index < numbers.length; index+= 1){
+    if(numbers[index] > valorMaior){
+        valorMaior = numbers[index];
+    }
+}
+console.log(valorMaior);
