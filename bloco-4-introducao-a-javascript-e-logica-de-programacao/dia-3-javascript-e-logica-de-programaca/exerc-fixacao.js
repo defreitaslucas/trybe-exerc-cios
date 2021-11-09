cd2dd007e5ddeaa38d847f74dae822dbd4139217
LucasTapa <lucas.dfa@live.com>
2021-11-09 19:16:43 -0300

    exercicio 1 e 2 do final do modulo

---
+++ bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-3-javascript-e-logica-de-programaca/exerc-fixacao.js
@@ -0,0 +1,19 @@
+// let n = 5;
+
+// for(let linha = 0; linha < n; linha += 1){
+//     let asterisco = ""
+//     for(let coluna = 0; coluna <= linha ; coluna+= 1){
+//         asterisco = asterisco + "*"
+//     }
+//     console.log(asterisco)
+// }
+
+
+// let n = 5;
+// let asterisco = "";
+// for(let coluna = 0; coluna < n ; coluna+= 1){
+//         asterisco += "*";
+// }
+// for(let linha = 0; linha < n; linha += 1){
+//     console.log(asterisco);
+// }
