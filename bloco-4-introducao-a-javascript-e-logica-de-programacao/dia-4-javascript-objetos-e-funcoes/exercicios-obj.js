d0b202dc6e479983427654471c9121630adc5deb
LucasTapa <lucas.dfa@live.com>
2021-11-10 18:36:47 -0300

    exercicios obj - 2,3,4,5

--- bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-4-javascript-objetos-e-funcoes/exercicios-obj.js
+++ bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-4-javascript-objetos-e-funcoes/exercicios-obj.js
@@ -3,5 +3,33 @@ let info = {
     origem: 'Pato Donald',
     nota: 'Namorada do personagem principal nos quadrinhos do Pato Donald',
   };
+//exercicio 1
+//   console.log("Bem vinda, "+info.personagem)
 
-  console.log("Bem vinda, "+info.personagem)
+//exercicio 2
+ info.recorrente = 'Sim'
+ //console.table(info)
+
+//exercicio 3
+// for (let keys in info){
+//     console.log(keys)
+// }
+
+//exercicio 4
+// for (let keys in info){
+//     console.log(info[keys])
+// }
+
+//exercicio 5
+let info2 = {
+    personagem: 'Tio Patinhas',
+    origem: "Christmas on Bear Mountain, Dell's Four Color Comics #178",
+    nota: 'O último MacPatinhas',
+    recorrente: 'Sim'
+}
+console.log(info.personagem+ " e " +info2.personagem)
+console.log(info.origem+ " e " +info2.origem)
+console.log(info.nota+ " e " +info2.nota)
+if (info.recorrente == 'Sim' && info2.recorrente == 'Sim'){
+    console.log("Ambos recorrentes")
+}
