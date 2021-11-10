843737c9280d350809b3cd4281eac98ed0e8a435
LucasTapa <lucas.dfa@live.com>
2021-11-10 15:02:52 -0300

    a

--- bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-3-javascript-e-logica-de-programaca/exerc-fixacao.js
+++ bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-3-javascript-e-logica-de-programaca/exerc-fixacao.js
@@ -1,6 +1,7 @@
 //imprime 5 linhas com 5 colunas de asteriscos
 // let n = 5;
 // let asterisco = "";
+
 // for(let coluna = 0; coluna < n ; coluna+= 1){
 //         asterisco += "*";
 // }
@@ -8,8 +9,8 @@
 //     console.log(asterisco);
 // }
 
-//imprime piramide de asteriscos
 
+//imprime piramide de asteriscos
 // let n = 5;
 
 // for(let linha = 0; linha < n; linha += 1){
@@ -21,38 +22,32 @@
 // }
 
 
-
-let n = 5;
-let espacoLinha = "";
-let posicao = n;
-
-for(let linha = 0; linha < n; linha += 1){
-    for(let coluna = 0; coluna <= n ; coluna+= 1){
-        if(coluna < posicao){
-            espacoLinha += " ";
-        } else {
-            espacoLinha += "*";
-        }
-    }
-    console.log(espacoLinha);
-    espacoLinha = ""
-    posicao -=1;
-}
-
+// imprime piramide lado oposto
 // let n = 5;
-// let symbol = '*';
-// let inputLine = '';
-// let inputPosition = n;
+// let espacoLinha = "";
+// let posicao = n;
 
-// for (let lineIndex = 0; lineIndex < n; lineIndex += 1) {
-//   for (let columnIndex = 0; columnIndex <= n; columnIndex += 1) {
-//     if (columnIndex < inputPosition) {
-//       inputLine = inputLine + ' ';
-//     } else {
-//       inputLine = inputLine + symbol;
+// for(let linha = 0; linha < n; linha += 1){
+//     for(let coluna = 0; coluna <= n ; coluna+= 1){
+//         if(coluna < posicao){
+//             espacoLinha += " ";
+//         } else {
+//             espacoLinha += "*";
+//         }
 //     }
-//   }
-//   console.log(inputLine);
-//   inputLine = '';
-//   inputPosition -= 1;
-// };
+//     console.log(espacoLinha);
+//     espacoLinha = ""
+//     posicao -=1;
+// }
+
+let symbol = '*'
+let n = 5;
+let resultado = "";
+
+for(let lin = 0; lin < n; lin += 1){
+    for(let col = 0; col < n; col += 1){
+        resultado = resultado + " ";
+    }
+    
+    console.log(resultado)
+}
