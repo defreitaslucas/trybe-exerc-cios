6294665e164b01791a710d30712e2314003418b7
LucasTapa <lucas.dfa@live.com>
2021-11-10 20:34:13 -0300

    exercicios obj - for e in

--- bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-4-javascript-objetos-e-funcoes/exercicios-obj.js
+++ bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-4-javascript-objetos-e-funcoes/exercicios-obj.js
@@ -1,13 +1,13 @@
-let info = {
-    personagem: 'Margarida',
-    origem: 'Pato Donald',
-    nota: 'Namorada do personagem principal nos quadrinhos do Pato Donald',
-  };
+// let info = {
+//     personagem: 'Margarida',
+//     origem: 'Pato Donald',
+//     nota: 'Namorada do personagem principal nos quadrinhos do Pato Donald',
+//   };
 //exercicio 1
 //   console.log("Bem vinda, "+info.personagem)
 
 //exercicio 2
- info.recorrente = 'Sim'
+ //info.recorrente = 'Sim'
  //console.table(info)
 
 //exercicio 3
@@ -21,15 +21,44 @@ let info = {
 // }
 
 //exercicio 5
-let info2 = {
-    personagem: 'Tio Patinhas',
-    origem: "Christmas on Bear Mountain, Dell's Four Color Comics #178",
-    nota: 'O último MacPatinhas',
-    recorrente: 'Sim'
-}
-console.log(info.personagem+ " e " +info2.personagem)
-console.log(info.origem+ " e " +info2.origem)
-console.log(info.nota+ " e " +info2.nota)
-if (info.recorrente == 'Sim' && info2.recorrente == 'Sim'){
-    console.log("Ambos recorrentes")
-}
+// let info2 = {
+//     personagem: 'Tio Patinhas',
+//     origem: "Christmas on Bear Mountain, Dell's Four Color Comics #178",
+//     nota: 'O último MacPatinhas',
+//     recorrente: 'Sim'
+// }
+// console.log(info.personagem+ " e " +info2.personagem)
+// console.log(info.origem+ " e " +info2.origem)
+// console.log(info.nota+ " e " +info2.nota)
+// if (info.recorrente == 'Sim' && info2.recorrente == 'Sim'){
+//     console.log("Ambos recorrentes")
+// }
+
+
+let leitor = {
+    nome: 'Julia',
+    sobrenome: 'Pessoa',
+    idade: 21,
+    livrosFavoritos: [
+      {
+        titulo: 'O Pior Dia de Todos',
+        autor: 'Daniela Kopsch',
+        editora: 'Tordesilhas',
+      },
+    ],
+  };
+//exercicio 6
+//console.log("O livro favorito de "+leitor.nome+ " "+ leitor.sobrenome+ " se chama "+ leitor['livrosFavoritos'][0]['titulo'])
+
+//exercicio 7
+leitor.livrosFavoritos.push(
+     {
+        titulo: 'Harry Potter e o Prisioneiro de Azkaban',
+        autor: 'JK Rowling',
+        editor: 'Rocco',
+    }
+)
+// console.log(leitor)
+
+//exercicio 8
+console.log(leitor.nome+ " tem "+leitor.livrosFavoritos.length+ " livros favoritos")
