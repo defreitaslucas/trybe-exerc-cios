345880c880836e7185177cc08d3ca24dcccc5509
LucasTapa <lucas.dfa@live.com>
2021-11-09 23:49:22 -0300

    exercicio 3 final do modulo

--- bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-3-javascript-e-logica-de-programaca/exerc-fixacao.js
+++ bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-3-javascript-e-logica-de-programaca/exerc-fixacao.js
@@ -1,3 +1,15 @@
+//imprime 5 linhas com 5 colunas de asteriscos
+// let n = 5;
+// let asterisco = "";
+// for(let coluna = 0; coluna < n ; coluna+= 1){
+//         asterisco += "*";
+// }
+// for(let linha = 0; linha < n; linha += 1){
+//     console.log(asterisco);
+// }
+
+//imprime piramide de asteriscos
+
 // let n = 5;
 
 // for(let linha = 0; linha < n; linha += 1){
@@ -9,11 +21,38 @@
 // }
 
 
+
+let n = 5;
+let espacoLinha = "";
+let posicao = n;
+
+for(let linha = 0; linha < n; linha += 1){
+    for(let coluna = 0; coluna <= n ; coluna+= 1){
+        if(coluna < posicao){
+            espacoLinha += " ";
+        } else {
+            espacoLinha += "*";
+        }
+    }
+    console.log(espacoLinha);
+    espacoLinha = ""
+    posicao -=1;
+}
+
 // let n = 5;
-// let asterisco = "";
-// for(let coluna = 0; coluna < n ; coluna+= 1){
-//         asterisco += "*";
-// }
-// for(let linha = 0; linha < n; linha += 1){
-//     console.log(asterisco);
-// }
+// let symbol = '*';
+// let inputLine = '';
+// let inputPosition = n;
+
+// for (let lineIndex = 0; lineIndex < n; lineIndex += 1) {
+//   for (let columnIndex = 0; columnIndex <= n; columnIndex += 1) {
+//     if (columnIndex < inputPosition) {
+//       inputLine = inputLine + ' ';
+//     } else {
+//       inputLine = inputLine + symbol;
+//     }
+//   }
+//   console.log(inputLine);
+//   inputLine = '';
+//   inputPosition -= 1;
+// };
