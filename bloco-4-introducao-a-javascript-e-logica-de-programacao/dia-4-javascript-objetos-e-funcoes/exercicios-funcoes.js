543669174c8fe4b6198c5d42704102ae9b1570d2
LucasTapa <lucas.dfa@live.com>
2021-11-11 13:53:36 -0300

    exercicios funcoes 4,5

--- bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-4-javascript-objetos-e-funcoes/exercicios-funcoes.js
+++ bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-4-javascript-objetos-e-funcoes/exercicios-funcoes.js
@@ -26,14 +26,47 @@
 // console.log(maiorValor([2,4,6,7,10,1]))
 
 //exercicio função 3
-function menorValor(arr){
-    console.log(arr);
-    let menor = 0;
-    for(let index in arr){
-        if(arr[index] < arr[menor]){
-            menor = index
+// function menorValor(arr){
+//     console.log(arr);
+//     let menor = 0;
+//     for(let index in arr){
+//         if(arr[index] < arr[menor]){
+//             menor = index
+//         }
+//     }
+//     return menor;
+// }
+// console.log(menorValor([2,4,6,7,10,0,-3]))
+
+//exercicio função 4
+//declaração da função
+// function nomeMaior(nomes){
+//     //starta a variavel com a primeira posição do array
+//     let nomeMaior = nomes[0];
+//     //cria a repetição
+//     for (let index in nomes){
+//         //o for na primeira repitição verifica se o a posição 0 e 0 são menores e assim vai sucessivamente crescendo o index
+//         if(nomeMaior.length < nomes[index].length){
+//             nomeMaior = nomes[index]
+//         }
+//     }
+//     return nomeMaior;
+// }
+// console.log(nomeMaior(['José', 'Lucas', 'Nádia', 'Fernanda', 'Cairo', 'Joana']))
+
+//exercicio função 5
+function maisRepete(numeros){
+    let repete = null;
+    let contador = 0;
+    for (let index in numeros){
+        if(repete !== numeros[index]){
+            repete = numeros[index];
+            contador += 1;
+        }
+        else{
+            contador += 1;
         }
     }
-    return menor;
+    return repete;
 }
-console.log(menorValor([2,4,6,7,10,0,-3]))
+console.log(maisRepete([2, 3, 2, 5, 8, 2, 3]))
